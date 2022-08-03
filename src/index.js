08fc333fa8819d1fe685744048ce44c5daf4cba0
Rachel Katz <rachel.katz@flatironschool.com>
2022-08-03 11:40:35 -0400

    bonus deliverables

--- src/index.js
+++ src/index.js
@@ -19,11 +19,16 @@ const reviewList = document.querySelector('#review-list')
 const descriptionForm = document.querySelector('#description-form')
 const reviewForm = document.querySelector('#review-form')
 
+//current loaded cake
+let curCakeId;
+
 /*
 *
 FUNCTION DEFINITIONS
 *
 */
+
+/*CAKE FUNCTIONS*/
 
 //get all cakes from db
 const getAllCakes = async function(){
@@ -47,32 +52,66 @@ const renderCake = function(cake) {
 
 //show cake details in info section
 const showCake = function(cake) {
+    curCakeId = cake.id;
     cakeName.innerText = cake.name;
     cakeDes.innerText = cake.description;
     cakeImage.src = cake.image_url;
     reviewList.innerHTML = '';
     cake.reviews.forEach((review) => {
-        createReview(review);
+        createReview(review); //create review elements for each review
     })
 }
 
-//add review from form to review list
-const addNewReview = function(form) {
-    createReview(form.review.value)
-    form.reset();
+
+//update description of cake with fetch PATCH
+const updateDescription = async function(form) {
+    let res = await fetch(`${url}/${curCakeId}`, {
+        method: 'PATCH', 
+        body: JSON.stringify({ description: form.description.value }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    let data = await res.json()
+    cakeDes.innerText = data.description
+    form.clear();
 }
 
-//create review in reviewList including on click delete
+/*REVIEW FUNCTIONS*/
+
+//update review list when adding new review, patch to db
+const patchReview = async function(form) {
+    //get request for current cake's reviews array
+    let resGet = await fetch(`${url}/${curCakeId}`)
+    let dataGet = await resGet.json();
+    let curReviews = dataGet.reviews;
+
+    //make post request
+    let resPatch = await fetch(`${url}/${curCakeId}`, {
+        method: 'PATCH', 
+        body: JSON.stringify({ reviews: [...curReviews, form.review.value]}), 
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    let dataPatch = await resPatch.json();
+    form.reset();
+    //pass in last element of reviews array
+    createReview(dataPatch.reviews[dataPatch.reviews.length - 1])
+}
+
+//create one review in reviewList, including on click delete
 const createReview = function(reviewText) {
     let newListEl = document.createElement('li');
     newListEl.innerText = reviewText;
 
-    //on click remove review
+    //on click remove review from DOM 
     newListEl.addEventListener('click', (e) => {
         e.target.remove();
     })
     reviewList.append(newListEl);
 }
+
 
 /*
 *
@@ -90,8 +129,14 @@ document.addEventListener('DOMContentLoaded', async () => {
     showCake(data);
 })
 
-//add submit event to review form
+//submit event to add review
 reviewForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    addNewReview(e.target);
+    patchReview(e.target);
+})
+
+//submit event to update description 
+descriptionForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    updateDescription(e.target);
 })
